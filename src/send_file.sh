abfa9330759f17497e24630f9334ddd5a4b6a8c6
Baz2013 <chuan.gu6@gmail.com>
2015-07-06 19:38:17 +0800

    new file:   .gitignore 	new file:   src/send_file.sh
    	新增.gitignore,send_file.sh脚本

---
+++ src/send_file.sh
@@ -0,0 +1,125 @@
+#!/bin/sh
+#文件分发脚本,实现将指定目录下的文件分发到其他主机的目录下 
+
+if [ $# -ne "2" ];then
+	echo "param wrong"
+	exit
+fi
+
+set -A host 0 10.124.0.31 10.124.0.31 10.124.0.31 10.124.0.31 10.124.0.31 10.124.0.32 10.124.0.32 10.124.0.32 10.124.0.32 10.124.0.32
+set -A users 0 billing1 billing2 billing3 billing4 billing5 billing6 billing7 billing8
+set -A arr_prov 0 11 34 38 86 97 36 13 31 87 51 81 59 79 91 74 89 75 76 10 85 88 17 70 18 71 83 19 90 84 30 50
+set -A arr_area 0 4 5 4 4 4 2 3 5
+
+password="billing@123"
+
+src_path="${1}"
+prov_code="${2}"
+cur_path=$(pwd)
+prov_no=""
+area_no=""
+serial_no=""
+dst_host=""
+dst_user=""
+dst_path=""
+
+writeLog(){
+	cur_time=$(date +"%Y-%m-%d %H:%M:%S")
+	cur_date=$(date +"%Y%m%d")
+	log_name=${cur_date}"".log
+	printf "${cur_time}|$1|\n" >>${2}/send_file.${log_name}
+}
+
+check_param(){
+	if [ ! -f "${1}" ];then
+		writeLog "文件${1} 不存在 !! " "${cur_path}"
+		echo "文件${1} 不存在 !! "
+		exit
+	fi
+
+	i=1
+	while [ ${i} -le 31 ]
+	do
+		if [ ${arr_prov[i]} -eq ${2} ];then
+			prov_no=${i}
+			break
+		fi      
+		let i=i+1
+	done
+
+	##echo "${prov_no}"
+	if [ ! -n "${prov_no}" ];then
+		echo "prov_code ${prov_code} 不存在 !"
+		writeLog "prov_code ${prov_code} 不存在 !" "${cur_path}"
+		exit
+	fi
+}
+
+get_area(){
+	i=1
+	k=0
+	while [ ${i} -lt 9 ]
+	do
+		#echo "${arr_area[i]}"
+		j=1
+		while [ ${j} -le ${arr_area[i]} ]
+		do
+			let k=k+1
+			if [ ${k} -eq ${prov_no} ];then
+				#echo "get it "
+				area_no=${i}
+				serial=${j}
+				break
+			fi
+			let j=j+1
+		done
+
+		if [ -n "${area_no}" -a -n "${serial}" ];then
+			break
+		fi
+		let i=i+1
+	done
+}
+
+get_dst(){
+	dst_host="${host[area_no]}"
+	dst_user="${users[area_no]}"
+	#echo "${dst_host}"
+	#echo "${dst_user}"
+	send_cmd="scp ${src_path} ${dst_user}@${dst_host}:\${BOSS_DATA${area_no}}/rr/it${serial}00/"
+}
+
+send_file(){
+	rm cmd.sh
+	echo "#!/usr/bin/expect -f
+	set USER [lindex \$argv 0]
+	set HOST [lindex \$argv 1]
+	set PASD [lindex \$argv 2]
+	set COMD [lindex \$argv 3]
+	spawn scp ${src_path} \${USER}@\${HOST}:/ngbss/billdata/data${area_no}/rr/it${serial}00/
+	expect \"*assword:\"
+	send \"\${PASD}\\\r\"
+	expect '#'
+	expect eof
+	" >>cmd.sh
+
+	chmod 777 cmd.sh
+	#echo "./cmd.sh ${dst_user} ${dst_host} ${password}"
+
+	./cmd.sh ${dst_user} ${dst_host} ${password} 2>/dev/null
+}
+
+clean_file(){
+	sleep  2
+	rm -f cmd.sh
+}
+
+check_param "${src_path}" "${prov_code}"
+get_area
+#echo "${area_no} ${serial}"
+
+get_dst
+#echo "${send_cmd}"
+send_file
+clean_file
+
