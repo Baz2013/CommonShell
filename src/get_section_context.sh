d2d4969cdf8e0830cd4086dcb9def924032f5249
Baz2013 <chuan.gu6@gmail.com>
2017-09-07 09:35:17 +0800

    refactor according to shellcheck

--- src/get_section_context.sh
+++ src/get_section_context.sh
@@ -2,9 +2,10 @@
 #获取指定标签里的内容,如<51000>....</51000>标签下的所有行,包括标签
 #getremote_channels.txt中内容为<51000>\n<51010>....
 
-while read line
+while read -r line
 do
-end_line=$(echo ${line}|sed 's/</<\\\//g') #将<51000>替换为</51000>
+# 将<51000>替换为</51000>
+end_line=$(echo "${line}"|sed 's/</<\\\//g')
 #echo "${line}"
 #echo "${end_line}"
 #echo "/${line}/,/${end_line}/{;p}"
